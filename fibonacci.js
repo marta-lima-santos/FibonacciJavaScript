3f7bdd46e96ab2927a58acca47939ccf7746cf42
mah-deve <martart.lune@gmail.com>
2023-06-09 16:10:21 -0300

    first commit

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+//Search Fibonacci 
+const number = parseInt(prompt("Enter a number: "));
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log("Fibonacci Series: ");
+console.log(n1);
+console.log(n2);
+
+nextTerm = n1 + n2;
+
+while(nextTerm <= number){
+     //print the next term
+     console.log(nextTerm);
+
+     n1 = n2;
+     n2 = nextTerm;
+     nextTerm = n1 + n2;
+}
